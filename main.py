5886e9fa713bf5fd00d7f627f5a25035b438be92
YanisBelkacemi <108769998+YanisBelkacemi@users.noreply.github.com>
2026-03-07 16:10:55 +0100

    cleaning mainfile

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from request import prompting
 prompt = input("enter the prompt")
 ollama_url = 'http://localhost:11434/api/generate'
 
-payload = {'model' : 'gemma3' , 'prompt' : 'why the sky is blue?'}
-
-resp = requests.post(ollama_url , payload)
-
 app = FastAPI()
 
 @app.get('/')
